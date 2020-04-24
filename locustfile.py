117ed41a92480c1c73844c3f6ee650ff9bbf478f
GitWahome <brianwahome254@gmail.com>
2020-04-23 22:49:39 -0700

    with templates

--- locustfile.py
+++ locustfile.py
@@ -8,17 +8,10 @@ class OverloadaAPI(TaskSet):
     @task(1)
     def load_api(self):
         # POST to login page with csrftoken
-        payload = [
-                        {
-                            "todo": "Some data",
-                            "todo_due_date": "2020-04-15",
-                            "todo_status": "todo",
-                            "user_id": "bgw254"
-                        }
-                    ]
+        payload = [{ "todo": "Some data","todo_due_date": "2020-04-15","todo_status": "todo","user_id": "bgw254"}]
 
         self.client.get(
-            url= f'api/v2/todos_add/?items={payload}'
+            url= 'api/v2/todos_add/?items=[{ "todo": "Some data","todo_due_date": "2020-04-15","todo_status": "todo","user_id": "bgw254"}]'
         )
 
 
