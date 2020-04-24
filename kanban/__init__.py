99fb198a7290dd709cad3782a4f2565c556beb7f
GitWahome <brianwahome254@gmail.com>
2020-04-23 21:32:38 -0700

    with templates

--- kanban/__init__.py
+++ kanban/__init__.py
@@ -7,7 +7,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="./static",template_folder="./templates")
 api = FlaskAPI(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
